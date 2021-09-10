462b36f2d34c4a0fc24657d8434ace5385f9a943
Janis Schanbacher <janis.schanbacher@posteo.de>
2021-09-10 21:11:35 +0200

    Add bulk configuration

--- frontend/src/components/configurationModule/ConfigurationModule.js
+++ frontend/src/components/configurationModule/ConfigurationModule.js
@@ -2,12 +2,11 @@ import React, { useState, useReducer } from 'react';
 import { Typography, Table, Button, Checkbox, Divider } from 'antd';
 import axios from "axios";
 import qs from 'qs'
-import { find, findIndex } from 'lodash';
+import { find } from 'lodash';
 
 import { apiUrl } from "../../helper/url";
 import CodeSelection from '../codeSelection/CodeSelection';
 
-const CheckboxGroup = Checkbox.Group;
 const { Title } = Typography;
 
 const plainOptions = ['Anlagengröße', 'Nutzungsgrad', 'Temperaturdifferenz', 'Rücklauftemperatur'];
@@ -20,6 +19,7 @@ const ConfigurationModule = () => {
   const [treeData, setTreeData] = useState([])
   // eslint-disable-next-line no-unused-vars
   const [_, forceUpdate] = useReducer((x) => x + 1, 0); // TODO: remove
+  const [bulkSelection, setBulkSelection] = useState(defaultCheckedList)
 
   const handleChange = (code, list) => {
     const index = dataSource.findIndex((item) => item.key === code); 
@@ -27,6 +27,32 @@ const ConfigurationModule = () => {
     newDataSource[index].checkedList = list   
     setDataSource(newDataSource)
     forceUpdate(); // TODO: remove
+  };
+
+  const handleChangeBulkConfiguration = (list) => {
+    setBulkSelection(list)
+  };
+
+  
+  /**
+   * Apply configuration to local configs the the selected rows.
+   */
+  const handleApplyBulkConfiguration = () => {
+    const newDataSource = dataSource;
+    const selectedKeys = rowSelection.map(row => row.key)
+
+    for(var i = 0; i < newDataSource.length; i++) {
+      console.log(newDataSource[i]);
+      if(selectedKeys.includes(newDataSource[i].key)){
+        newDataSource[i].checkedList = bulkSelection;
+      }
+    }
+    setDataSource(newDataSource);
+    forceUpdate()
+
+    // forceUpdate();
+    // newDataSource[index].checkedList = list   
+       // forceUpdate(); // TODO: remove
   };
 
   // TODO: evt. auch einfach alle anlagen darstellen, und nach coedes gruppieren, die dann ausklappbar sind. https://ant.design/components/table/#components-table-demo-tree-data 
@@ -41,7 +67,7 @@ const ConfigurationModule = () => {
       // editable: true,
       render: (val, row) => {    
         const index = dataSource.findIndex((item) => item.key === row.key); 
-        return <CheckboxGroup options={plainOptions} value={dataSource[index].checkedList} onChange={(list) => handleChange(row.key, list)} />
+        return <Checkbox.Group options={plainOptions} value={dataSource[index].checkedList} onChange={(list) => handleChange(row.key, list)} />
       }
     },
     {
@@ -55,15 +81,12 @@ const ConfigurationModule = () => {
   ];
 
   const loadConfigs = () => {
-    console.log(value);
     const codes = value.filter(v => v.value.length > 3).map(v => v.label)
     // Add codes of children from selected prefix (f.i. {value: "0-1", value: "ACO"})
     const keysOfPrefixes = value.filter(v => v.value.length === 3)
     for(var i = 0; i < keysOfPrefixes.length ; i++) {
       const treeElement = find(treeData, o => o.value === keysOfPrefixes[i].value)
-      console.log("treeElement: " + treeElement);
       if(treeElement !== undefined && treeElement.children !== undefined){
-        console.log(treeElement);
         for(var k = 0; k < treeElement.children.length ; k++){
           codes.push(treeElement.children[k].title);
         } 
@@ -81,7 +104,6 @@ const ConfigurationModule = () => {
         })
     .then((res) => {
         const { data } = res;
-        console.log(rowSelection);
         const configs = Object.keys(data).map(key => {
             var activeAnalyses = [];
             if(data[key].facilitySize){
@@ -104,14 +126,10 @@ const ConfigurationModule = () => {
         })
 
         // add default config, if non exists yet
-        // const keysOfRetrievedConfigs = configs.map(o => o.key)
-        // console.log("keysOfRetrievedConfigs: " + keysOfRetrievedConfigs);
         for(var i = 0; i < codes.length; i++) {
-          console.log(configs);
           const index =  configs.findIndex(c => c.code === codes[i].toUpperCase()) 
-          console.log("index: " + index);
           if(index < 0){ 
-            console.log("Adding default for: " + codes[i]);
+            // console.log("Adding default for: " + codes[i]);
             configs.push({key: codes[i], code: codes[i], checkedList: defaultCheckedList})
           }
         }
@@ -177,14 +195,40 @@ const ConfigurationModule = () => {
           onClick={loadConfigs}
           type="primary"
           style={{
-              margin: "5px 5px 15px 5px",
+              margin: "10px 0",
               float: 'left'
           }} // TODO: use StyledComponend
         >
         Konfigurationen laden
         </Button>
-        <Divider />  
+        <Divider />
+        <Title level={4} style={{textAlign: "left"}}>Markierte Anlagen Konfigurieren</Title>
+        <Checkbox.Group
+          options={plainOptions} 
+          defaultValue={defaultCheckedList}
+          onChange={handleChangeBulkConfiguration}
+          style={{textAlign: "left", width: "100%", margin: "10px 0"}}
+        />
+        <Button  
+          onClick={handleApplyBulkConfiguration} 
+          type="primary"
+          style={{
+              margin: "10px 0",
+              float: 'left'
+          }} // TODO: use StyledComponend
+        >
+        Übernehmen
+        </Button>
+        <Divider />
         <Title level={4} style={{textAlign: "left"}}>Konfigurationen</Title>
+        <Button
+          onClick={handleConfirmSelection}
+          type="primary"
+          style={{
+              margin: "10px 0",
+              float: 'left'
+          }} // TODO: use StyledComponend
+         >Auswahl anwenden</Button>
         <Table
          rowSelection={{
           type: "checkbox",
@@ -195,14 +239,6 @@ const ConfigurationModule = () => {
           dataSource={dataSource}
           columns={columnsRender}
         />
-        <Button
-          onClick={handleConfirmSelection}
-          type="primary"
-          style={{
-              margin: "5px 5px 15px 5px",
-              float: 'left'
-          }} // TODO: use StyledComponend
-         >Auswahl anwenden</Button>
     </div>
   );
 }
