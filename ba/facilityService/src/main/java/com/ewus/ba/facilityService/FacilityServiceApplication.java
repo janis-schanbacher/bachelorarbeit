7e49926597024e426b1d887b34e0abf10d90eb5a
Janis Schanbacher <janis.schanbacher@posteo.de>
2021-10-19 10:55:13 +0200

    Remove Eureka discovery server, and eureka client configs.

--- ba/facilityService/src/main/java/com/ewus/ba/facilityService/FacilityServiceApplication.java
+++ ba/facilityService/src/main/java/com/ewus/ba/facilityService/FacilityServiceApplication.java
@@ -2,10 +2,8 @@ package com.ewus.ba.facilityService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class FacilityServiceApplication {
 
   public static void main(String[] args) {
